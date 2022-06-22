c91cf08d5c3293e2fa3e1d35a38c7d9dd094d9c8
slb19xx <93225634+slb19xx@users.noreply.github.com>
2022-06-22 14:28:13 -0400

    Moved forecast HTML to JS string

--- src/app.js
+++ src/app.js
@@ -22,6 +22,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row" >`;
+
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="36"/>
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18°</span> 
+                  <span class="weather-forecast-temperature-min">12°</span></div>
+            </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -95,3 +121,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("New York");
+displayForecast();
